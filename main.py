d45f95559faea8868a27b65ba8d307a70c7bb172
tyushnyakov <ktyushnyakov@gmail.com>
2022-05-25 19:29:19 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,188 @@
+#
+#
+# with open('./file.txt', 'r', encoding='utf-8') as file:
+#     list_goods = file.readlines()
+#
+#     mean = 0
+#     count_all = 0
+#     summ = 0
+#     max_price = 0
+#     min_price = 1000
+#     max_index = 0
+#     min_index = 0
+#
+#     set_goods = set()
+#
+#     for good in list_goods:
+#         price = int(good.split(':')[1])
+#
+#         summ += price
+#         count_all += 1
+#
+#         if price > 100:
+#             set_goods.add(good)
+#
+#         if price > max_price:
+#             max_price = price
+#             max_index = list_goods.index(good)
+#             print(f'максимальная цена: {max_price}')
+#             print(f'индекс максимальной цены: {max_index}')
+#
+#         if price < min_price:
+#             min_price = price
+#             min_index = list_goods.index(good)
+#             print(f'минимальная цена: {min_price}')
+#             print(f'индекс минимальной цены: {min_index}')
+#
+#
+#     dict_goods = {}
+#     dict_goods['mean'] = summ / count_all
+#     dict_goods['sum'] = summ
+#     dict_goods['count_all'] = count_all
+#
+#     print(dict_goods)
+#     print(set_goods)
+#
+#     def work_with_list(list_goods, is_max_min):
+#         list_result = []
+#         max = 0
+#         min = 999999
+#
+#         for good in list_goods:
+#             price = int(good.split(':')[1])
+#
+#             if price > max:
+#                 max = price
+#
+#             if price < min:
+#                 min = price
+#
+#         if is_max_min:
+#             for good in list_goods:
+#                 price = int(good.split(':')[1])
+#                 name = good.split(':')[0]
+#
+#                 if price == max:
+#                     list_result.append(name)
+#         else:
+#             for good in list_goods:
+#                 price = int(good.split(':')[1])
+#                 name = good.split(':')[0]
+#
+#                 if price == min:
+#                     list_result.append(name)
+#
+#         return list_result
+#
+#
+#     list_goods_max = work_with_list(list_goods, is_max_min=True)
+#     list_goods_min = work_with_list(list_goods, is_max_min=False)
+#
+#     print(list_goods_max)
+#     print(list_goods_min)
+
+
+class Good:
+    '''Это класс товара с именем, стоимостью и количеством'''
+
+    def __init__(self, name, count, price):
+        self.name = name
+        self.count = count
+        self.price = price
+
+
+class GoodList:
+    '''Это класс со списком товаров'''
+
+    def __init__(self):
+        self.good_list = []
+
+    def add_good_in_list(self, good: Good):
+        '''Добавляем товар в список'''
+
+        self.good_list.append(good)
+
+    def get_mean_price(self):
+        '''Получаем среднюю стоимость товаров'''
+
+        sum_price = 0
+        sum_count = 0
+
+        for good in self.good_list:
+            sum_price += int(good.price)
+            sum_count += int(good.count)
+
+        print(f'summ goods = {sum_price}')
+        print(f'count = {sum_count}')
+
+        mean = sum_price / sum_count
+        print(f'mean = {mean}')
+
+        return mean
+
+    def get_good_with_max_price(self):
+        '''Получаем товар с максимальной ценой'''
+
+        name = ''
+        max_price = 0
+
+        for good in self.good_list:
+            if good.price > max_price:
+                max_price = good.price
+                name = good.name
+
+        return name
+
+    def get_good_with_min_price(self):
+        '''Получаем товар с минимальной ценой'''
+
+        name = ''
+        min_price = 1000000
+
+        for good in self.good_list:
+            if good.price < min_price:
+                min_price = good.price
+                name = good.name
+
+        return name
+
+    def get_good_with_max_count(self):
+        '''Получаем товар с максимальным количеством'''
+
+        name = ''
+        max_count = 0
+
+        for good in self.good_list:
+            if good.count > max_count:
+                max_count = good.count
+                name = good.name
+
+        return name
+
+    def get_good_with_min_count(self):
+        '''Получаем товар с минимальным количеством'''
+
+        name = ''
+        min_count = 100
+
+        for good in self.good_list:
+            if good.count < min_count:
+                min_count = good.count
+                name = good.name
+
+        return name
+
+good_list = GoodList()
+
+with open('./file.txt', 'r') as file:
+    list_with_goods = file.readlines()
+
+    for str_good in list_with_goods:
+        list_good = str_good.split(':')
+
+        name = list_good[0]
+        price = list_good[1]
+        count = list_good[2]
+
+        good_list.add_good_in_list(Good(name, price, count))
+        good_list.get_mean_price()
